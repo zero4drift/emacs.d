3188784cbd91ce03b036d7c4fb8e52f415433202
Fang Jiancong <fang0052@e.ntu.edu.sg>
2017-07-12 16:07:45 +0800

    add recentf

--- lisp/init-defaults.el
+++ lisp/init-defaults.el
@@ -9,6 +9,9 @@
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 
+(recentf-mode 1)            
+(setq recentf-max-menu-items 25)
+
 (add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
 
 (electric-indent-mode t)
