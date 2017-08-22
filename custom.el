18edbdaa0e360c60f565989f74fff1375d7e3db2
Fang Jiancong <fang0052@e.ntu.edu.sg>
2017-08-22 18:04:20 +0800

    delete useless statement

--- custom.el
+++ custom.el
@@ -7,12 +7,14 @@
  ;; If there is more than one, they won't work right.
  '(company-idle-delay 0.1)
  '(company-minimum-prefix-length 1)
+ '(custom-safe-themes
+   (quote
+    ("23cf1bbd82721df1785aa1a10f742e555d6ea41921b65fab0345947bdd56c3f8" default)))
  '(package-selected-packages (quote (magit company))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (atom-one-dark)))
  '(default ((t (:family "文泉驿微米黑" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
  
