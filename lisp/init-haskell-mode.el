2aa72418d08add3e45fccafe3578cc59996f4907
Fang Jiancong <fang0052@e.ntu.edu.sg>
2017-08-23 17:20:03 +0800

    simplify the haskell-mode init.el

--- lisp/init-haskell-mode.el
+++ lisp/init-haskell-mode.el
@@ -2,9 +2,5 @@
 
 (require-package 'haskell-mode)
 (require 'haskell-mode)
-(require 'haskell-interactive-mode)
-(require 'haskell-process)
-(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-(add-to-list 'Info-default-directory-list "~/elpa/haskell-mode/")
 
 (provide 'init-haskell-mode)
