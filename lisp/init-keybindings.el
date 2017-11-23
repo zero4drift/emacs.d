59b43ff098c44cbd8d42164c10ddbbc5b5b2e155
zero4drift <fang0052@e.ntu.edu.sg>
2017-11-23 15:14:54 +0800

    Add a short cut to open the init.el.

--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -1,5 +1,14 @@
 ;; init-keybindgs.el
 
+;; bindings for general utilization
+
+;; shortcut to open init.el
+(defun open-init()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+(global-set-key (kbd "<f2>") 'open-init)
+
+;; bindings for magit
 (global-set-key (kbd "C-x g") 'magit-status)
 
 (provide 'init-keybindings)
