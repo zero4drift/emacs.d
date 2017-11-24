55e94cbb09ea0e55291be736dc3cc89b8def978f
zero4drift <fang0052@e.ntu.edu.sg>
2017-11-24 19:46:39 +0800

    Emacs's garbage collect on win 10 make itself really slow.

--- init.el
+++ init.el
@@ -6,6 +6,15 @@
 ;; You may delete these explanatory comments.
 ;; (package-initialize)
 
+;; 设置垃圾回收，在Windows下，emacs25版本会频繁出发垃圾回收，所以需要设置
+(when (eq system-type 'windows-nt)
+  (setq gc-cons-threshold (* 512 1024 1024))
+  (setq gc-cons-percentage 0.5)
+  (run-with-idle-timer 5 t #'garbage-collect)
+  ;; 显示垃圾回收信息，这个可以作为调试用
+  ;; (setq garbage-collection-messages t)
+  )
+
 (defun open-init()
   (interactive)
   (find-file (expand-file-name "init.el" user-emacs-directory)))
