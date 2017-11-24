92333cbf93bf551e926dfe85bb4f780d8a74fedf
zero4drift <fang0052@e.ntu.edu.sg>
2017-11-24 19:42:04 +0800

    Change font style to Microsoft Yahei Mono.

--- custom.el
+++ custom.el
@@ -15,5 +15,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
+ '(default ((t (:family "Microsoft Yahei Mono" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
  
