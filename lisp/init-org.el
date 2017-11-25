bd868047d38124591737bf2ce1ea320ae5522466
zero4drift <fang0052@e.ntu.edu.sg>
2017-11-25 23:16:11 +0800

    Try to add a org templates, .gitignore updated due to folder bookmarks, no key-binding added for now.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,8 +1,17 @@
 ;; init-org
 
 (require 'org)
+
 (setq org-src-fontify-natively t)
 
 (setq org-agenda-files '("~/org"))
+(setq org-capture-templates
+      '((
+	 "t"
+	 "Todo"
+	 entry
+	 (file+headline "~/org/gtd.org"  "工作安排")
+	 "* TODO [#B] %?\n %i\n"
+	 :empty-lines 1)))
 
 (provide 'init-org)
