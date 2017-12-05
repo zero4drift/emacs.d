93fb4f5463450486074d3272d8a92c3e7e5a447c
zero4drift <fang0052@e.ntu.edu.sg>
2017-12-05 13:33:45 +0800

    Finally rightful fonts applied to English and Chinese.

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -12,21 +12,17 @@
 ;; set font
 (setq fonts
       (cond ((eq system-type 'darwin)     '("Monaco"    "STHeiti"))
-            ((eq system-type 'gnu/linux)  '("Menlo"     "WenQuanYi Zen Hei"))
+            ((eq system-type 'gnu/linux)  '("DejaVu Sans Mono"     "WenQuanYi Zen Hei"))
             ((eq system-type 'windows-nt) '("Consolas"  "Microsoft Yahei"))))
+
 (set-face-attribute 'default nil :font
-                    (format "%s:pixelsize=%d" (car fonts) 14))
-(defun s-font ()
-  (interactive)
+		    (format "%s:pixelsize=%d" (car fonts) 14))
+
+(when (display-graphic-p)
   (dolist (charset '(kana han symbol cjk-misc bopomofo))
     (set-fontset-font (frame-parameter nil 'font)
 		      charset
 		      (font-spec :family (car (cdr fonts))))))
-(add-to-list 'after-make-frame-functions
-	     (lambda (new-frame)
-	       (select-frame new-frame)
-	       (if window-system
-		   (s-font))))
 
 ;; Fix chinese font width and rescale
 (setq face-font-rescale-alist '(("Microsoft Yahei" . 1.2) ("WenQuanYi Micro Hei Mono" . 1.2) ("STHeiti". 1.3)))
