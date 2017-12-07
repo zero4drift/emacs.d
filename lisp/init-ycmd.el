d740a6737a972541f1a4692fb962ac968dce63b8
zero4drift <fang0052@e.ntu.edu.sg>
2017-12-07 17:36:29 +0800

    Gradually complete emacs-ycmd configurations.

--- lisp/init-ycmd.el
+++ lisp/init-ycmd.el
@@ -4,5 +4,7 @@
 (require 'ycmd)
 
 (add-hook 'c++-mode-hook 'ycmd-mode)
+;; "~/github/ycmd/ycmd" is where my built ycmd package locates on ubuntu.
+(set-variable 'ycmd-server-command `("python" ,(file-truename "~/github/ycmd/ycmd/")))
 
 (provide 'init-ycmd)
