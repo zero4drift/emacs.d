2326743350431f62e957e978f4e001252aa7ced5
zero4drift <fang0052@e.ntu.edu.sg>
2017-12-16 14:50:13 +0800

    Make modeline more reliable and beautiful.

--- lisp/init-defaults.el
+++ lisp/init-defaults.el
@@ -1,5 +1,115 @@
 ;; init-defaults.el
 
+;; modeline
+(defun mode-line-fill (face reserve)
+  "Return empty space using FACE and leaving RESERVE space on the right."
+  (unless reserve
+    (setq reserve 20))
+  (when (and window-system (eq 'right (get-scroll-bar-mode)))
+    (setq reserve (- reserve 3)))
+  (propertize " "
+	      'display `((space :align-to
+				(- (+ right right-fringe right-margin) ,reserve)))
+	      'face face))
+
+(defun buffer-encoding-abbrev ()
+  "The line ending convention used in the buffer."
+  (let ((buf-coding (format "%s" buffer-file-coding-system)))
+    (if (string-match "\\(dos\\|unix\\|mac\\)" buf-coding)
+	(match-string 1 buf-coding)
+      buf-coding)))
+
+(setq my-flycheck-mode-line
+      '(:eval
+	(pcase flycheck-last-status-change
+	  (`not-checked nil)
+	  (`no-checker (propertize " -" 'face 'warning))
+	  (`running (propertize " ✷" 'face 'success))
+	  (`errored (propertize " !" 'face 'error))
+	  (`finished
+	   (let* ((error-counts (flycheck-count-errors flycheck-current-errors))
+		  (no-errors (cdr (assq 'error error-counts)))
+		  (no-warnings (cdr (assq 'warning error-counts)))
+		  (face (cond (no-errors 'error)
+			      (no-warnings 'warning)
+			      (t 'success))))
+	     (propertize (format "[%s/%s]" (or no-errors 0) (or no-warnings 0))
+			 'face face)))
+	  (`interrupted " -")
+	  (`suspicious '(propertize " ?" 'face 'warning)))))
+
+(setq-default mode-line-format
+	      (list
+	       "%1 "
+	       ;; the buffer name; the file name as a tool tip
+	       '(:eval (propertize "%b " 'face 'font-lock-keyword-face
+				   'help-echo (buffer-file-name)))
+	       
+	       " [" ;; insert vs overwrite mode, input-method in a tooltip
+	       '(:eval (propertize (if overwrite-mode "Ovr" "Ins")
+				   'face 'font-lock-preprocessor-face
+				   'help-echo (concat "Buffer is in "
+						      (if overwrite-mode
+							  "overwrite"
+							"insert") " mode")))
+
+	       ;; was this buffer modified since the last save?
+	       '(:eval (when (buffer-modified-p)
+			 (concat ","  (propertize "Mod"
+						  'face 'font-lock-warning-face
+						  'help-echo "Buffer has been modified"))))
+
+	       ;; is this buffer read-only?
+	       '(:eval (when buffer-read-only
+			 (concat ","  (propertize "RO"
+						  'face 'font-lock-type-face
+						  'help-echo "Buffer is read-only"))))
+	       "] "
+
+	       ;; relative position, size of file
+	       "["
+	       (propertize "%p" 'face 'font-lock-constant-face) ;; % above top
+	       "/"
+	       (propertize "%I" 'face 'font-lock-constant-face) ;; size
+	       "] "
+
+	       ;; the current major mode for the buffer.
+	       '(:eval (propertize "%m" 'face 'font-lock-string-face
+				   'help-echo buffer-file-coding-system))
+
+	       "%1 "
+	       my-flycheck-mode-line
+	       "%1 "
+
+	       ;; minor modes but I feel that's too many
+	       ;; minor-mode-alist
+	       
+	       " "
+	       ;; git info
+	       `(vc-mode vc-mode)
+
+	       " "
+	       
+	       ;; global-mode-string goes in mode-line-misc-info
+	       mode-line-misc-info
+
+	       (mode-line-fill 'mode-line 20)
+
+	       ;; line and column
+	       "(" ;; '%02' to set to 2 chars at least; prevents flickering
+	       (propertize "%02l" 'face 'font-lock-type-face) ","
+	       (propertize "%02c" 'face 'font-lock-type-face)
+	       ") "
+
+	       '(:eval (buffer-encoding-abbrev))
+	       mode-line-end-spaces " "
+	       ;; add the time, with the date and the emacs uptime in the tooltip
+	       '(:eval (propertize (format-time-string "%H:%M")
+				   'help-echo
+				   (concat (format-time-string "%c; ")
+					   (emacs-uptime "Uptime:%hh"))))
+	       ))
+
 ;; make symbols look better
 (global-prettify-symbols-mode 1)
 
