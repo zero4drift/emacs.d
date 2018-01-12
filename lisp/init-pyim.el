4cdcf00ff572fab303d0a24c7147f6f6a9f67ca4
zero4drift <fang0052@e.ntu.edu.sg>
2018-01-12 11:56:17 +0800

    Complete pyim configuration, but could not be activated in swiper?

--- lisp/init-pyim.el
+++ lisp/init-pyim.el
@@ -17,7 +17,7 @@
 		pyim-probe-punctuation-after-punctuation))
 
 (pyim-isearch-mode 1)
-;; (setq pyim-page-tooltip 'popup)
+(setq pyim-page-tooltip 'popup)
 (setq pyim-page-length 5)
 (add-hook 'emacs-startup-hook
 	  #'(lambda() (pyim-restart-1 t)))
