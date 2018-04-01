9d2f486f51c497204c970028a43d01afb1ffa866
zero4drift <fang0052@e.ntu.edu.sg>
2018-04-01 22:42:22 +0800

    Customization for deft-mode.

--- init.el
+++ init.el
@@ -526,7 +526,18 @@
   :custom
   (deft-default-extension "org")
   (deft-directory "~/.org/deft/")
-  (deft-use-filename-as-title t))
+  (deft-recursive t)
+  (deft-file-naming-rules '((noslash . "_")))
+  (deft-text-mode 'org-mode)
+  (deft-use-filter-string-for-filename t)
+  (deft-org-mode-title-prefix t)
+  (deft-use-filename-as-title nil)
+  (deft-strip-summary-regexp
+	(concat "\\("
+		"[\n\t]" ;; blank
+		"\\|^#\\+[[:upper:]_]+:.*$" ;; org-mode metadata
+		"\\|^#\\+[[:alnum:]_]+:.*$" ;; org-mode metadata
+		"\\)")))
 
 ;; iedit
 (use-package iedit
