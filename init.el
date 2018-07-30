1eb17ffa5176b55f2eacb5c3fff2ed6e77a8a125
zero4drift <fang0052@e.ntu.edu.sg>
2018-07-30 15:06:52 +0800

    Move detailed wanderlust configures to file ~/.wl.

--- init.el
+++ init.el
@@ -769,15 +769,6 @@
 
 ;; mail-agent wanderlust
 (autoload 'wl "wl" "Wanderlust" t)
-(if (boundp 'mail-user-agent)
-    (setq mail-user-agent 'wl-user-agent))
-(if (fboundp 'define-mail-user-agent)
-    (define-mail-user-agent
-      'wl-user-agent
-      'wl-user-agent-compose
-      'wl-draft-send
-      'wl-draft-kill
-      'mail-send-hook))
 
 ;; modeline
 (defun mode-line-fill (face reserve)
