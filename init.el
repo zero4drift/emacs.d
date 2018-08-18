c4d64d2d39ac9c463f217749b6d65a23694a064e
zero4drift <fang0052@e.ntu.edu.sg>
2018-08-18 16:01:10 +0800

    Update to emacs 26.1 and config modification no.1.

--- init.el
+++ init.el
@@ -35,17 +35,6 @@
 (when (file-exists-p custom-file)
   (load custom-file))
 
-
-;; 设置垃圾回收，在Windows下，emacs25版本会频繁出发垃圾回收，所以需要设置
-(when (eq system-type 'windows-nt)
-  (setq gc-cons-threshold (* 512 1024 1024))
-  (setq gc-cons-percentage 0.5)
-  (run-with-idle-timer 5 t #'garbage-collect)
-  ;; 显示垃圾回收信息，这个可以作为调试用
-  ;; (setq garbage-collection-messages t)
-  )
-
-
 ;; ui
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
