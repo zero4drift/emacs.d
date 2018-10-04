db356ca97e4bca8093e1aaae92c4a9278b6650cd
zero4drift <fang0052@e.ntu.edu.sg>
2018-10-04 14:00:12 +0800

    Use treemacs instead of neotree.

--- init.el
+++ init.el
@@ -95,7 +95,7 @@
 (defun open-init()
   (interactive)
   (find-file (expand-file-name "init.el" user-emacs-directory)))
-(global-set-key (kbd "<f3>") 'open-init)
+(global-set-key (kbd "<f2>") 'open-init)
 
 ;; shortcut to find function, variable
 (global-set-key (kbd "C-h C-f") 'find-function)
@@ -544,7 +544,7 @@
     (find-file "~/.accounting/2018.ledger"))
   :ensure t
   :mode "\\.ledger$"
-  :bind (([f4] . open-ledger)))
+  :bind (([f3] . open-ledger)))
 
 ;; magit
 (use-package magit
@@ -640,12 +640,65 @@
   :defer t
   :hook (after-init . doom-modeline-init))
 
-;; neotree
-(use-package neotree
+;; treemacs
+(use-package treemacs
   :ensure t
+  :defer t
   :init
-  (setq neo-theme (if (display-graphic-p) 'icons))
-  :bind (([f2] . neotree-toggle)))
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :config
+  (progn
+    (setq treemacs-collapse-dirs              (if (executable-find "python") 3 0)
+          treemacs-deferred-git-apply-delay   0.5
+          treemacs-display-in-side-window     t
+          treemacs-file-event-delay           5000
+          treemacs-file-follow-delay          0.2
+          treemacs-follow-after-init          t
+          treemacs-follow-recenter-distance   0.1
+          treemacs-goto-tag-strategy          'refetch-index
+          treemacs-indentation                2
+          treemacs-indentation-string         " "
+          treemacs-is-never-other-window      nil
+          treemacs-no-png-images              nil
+          treemacs-project-follow-cleanup     nil
+          treemacs-persist-file               (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+          treemacs-recenter-after-file-follow nil
+          treemacs-recenter-after-tag-follow  nil
+          treemacs-show-hidden-files          t
+          treemacs-silent-filewatch           nil
+          treemacs-silent-refresh             nil
+          treemacs-sorting                    'alphabetic-desc
+          treemacs-space-between-root-nodes   t
+          treemacs-tag-follow-cleanup         t
+          treemacs-tag-follow-delay           1.5
+          treemacs-width                      35)
+
+    ;; The default width and height of the icons is 22 pixels. If you are
+    ;; using a Hi-DPI display, uncomment this to double the icon size.
+    ;;(treemacs-resize-icons 44)
+
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode t)
+    (pcase (cons (not (null (executable-find "git")))
+                 (not (null (executable-find "python3"))))
+      (`(t . t)
+       (treemacs-git-mode 'extended))
+      (`(t . _)
+       (treemacs-git-mode 'simple))))
+  :bind
+  (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
+        ("C-x t t"   . treemacs)
+        ("C-x t B"   . treemacs-bookmark)
+        ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag)))
+
+(use-package treemacs-projectile
+  :after treemacs projectile
+  :ensure t)
 
 ;; which-key
 (use-package which-key
@@ -712,10 +765,10 @@
   (("M-j" . pyim-convert-code-at-point) ;与 pyim-probe-dynamic-english 配合
    ("C-." . pyim-delete-word-from-personal-buffer)))
 
-;; window-numbering
-(use-package window-numbering
+;; winum
+(use-package winum
   :ensure t
-  :config (window-numbering-mode 1))
+  :config (winum-mode 1))
 
 ;; evil-nerd-commenter
 (use-package evil-nerd-commenter
@@ -729,7 +782,7 @@
 ;; begin evil conf
 ;; evil-leader
 (use-package evil-leader
-  :after (window-numbering)
+  :after (winum)
   :ensure t
   :config
   (global-evil-leader-mode)
@@ -774,7 +827,7 @@
 (use-package evil
   :ensure t
   :bind
-  (([f5] . evil-local-mode))
+  (([f4] . evil-local-mode))
   :hook ((prog-mode fundamental-mode text-mode)
 	 . evil-local-mode))
 ;; end evil confs
