7a41c95d01dcf236306a6062b0b70911dfa84471
zero4drift <fang0052@e.ntu.edu.sg>
2018-10-04 18:28:23 +0800

    Use hydra to make a cheatsheet for smartparens.

--- init.el
+++ init.el
@@ -561,49 +561,66 @@
 (use-package smartparens-config
   :ensure smartparens
   :config
-  (progn (show-smartparens-global-mode t)))
+  (progn (show-smartparens-global-mode t))
+  :hook
+  (prog-mode . turn-on-smartparens-strict-mode))
 
-(bind-keys
- :map smartparens-mode-map
- ("C-M-a" . sp-beginning-of-sexp)
- ("C-M-e" . sp-end-of-sexp)
+;; hydra
+(use-package hydra
+  :ensure t
+  :defer t)
 
- ("C-<down>" . sp-down-sexp)
- ("C-<up>"   . sp-up-sexp)
- ("M-<down>" . sp-backward-down-sexp)
- ("M-<up>"   . sp-backward-up-sexp)
-
- ("C-M-f" . sp-forward-sexp)
- ("C-M-b" . sp-backward-sexp)
-
- ("C-M-n" . sp-next-sexp)
- ("C-M-p" . sp-previous-sexp)
-
- ("C-S-f" . sp-forward-symbol)
- ("C-S-b" . sp-backward-symbol)
-
- ("C-<right>" . sp-forward-slurp-sexp)
- ("M-<right>" . sp-forward-barf-sexp)
- ("C-<left>"  . sp-backward-slurp-sexp)
- ("M-<left>"  . sp-backward-barf-sexp)
-
- ("C-M-t" . sp-transpose-sexp)
- ("C-M-k" . sp-kill-sexp)
- ("C-k"   . sp-kill-hybrid-sexp)
- ("M-k"   . sp-backward-kill-sexp)
- ("C-M-w" . sp-copy-sexp)
- ("C-M-d" . delete-sexp)
-
- ("M-<backspace>" . backward-kill-word)
- ("C-<backspace>" . sp-backward-kill-word)
- ([remap sp-backward-kill-word] . backward-kill-word)
-
- ("M-[" . sp-backward-unwrap-sexp)
- ("M-]" . sp-unwrap-sexp)
-
- ("C-x C-t" . sp-transpose-hybrid-sexp))
-
-(add-hook 'prog-mode-hook 'turn-on-smartparens-strict-mode)
+(defhydra hydra-smartparens (global-map "M-p" :hint t)
+  "
+Sexps (quit with _q_, help with _h_)
+^Nav^            ^Barf/Slurp^                 ^Depth^
+^---^------------^----------^-----------------^-----^-----------------
+_f_: forward     _→_:          slurp forward   _R_: splice
+_b_: backward    _←_:          barf forward    _r_: raise
+_u_: backward ↑  _C-<right>_:  slurp backward  _↑_: raise backward
+_d_: forward ↓   _C-<left>_:   barf backward   _↓_: raise forward
+_p_: backward ↓
+_n_: forward ↑
+^Kill^           ^Misc^                       ^Wrap^
+^----^-----------^----^-----------------------^----^------------------
+_w_: copy        _j_: join                    _(_: wrap with ( )
+_k_: kill        _s_: split                   _{_: wrap with { }
+^^               _t_: transpose               _'_: wrap with ' '
+^^               _c_: convolute               _\"_: wrap with \" \"
+^^               _i_: indent defun"
+  ("q" nil)
+  ("h" hydra-smartparens/body)
+  ;; Wrapping
+  ("(" (lambda (_) (interactive "P") (sp-wrap-with-pair "(")))
+  ("{" (lambda (_) (interactive "P") (sp-wrap-with-pair "{")))
+  ("'" (lambda (_) (interactive "P") (sp-wrap-with-pair "'")))
+  ("\"" (lambda (_) (interactive "P") (sp-wrap-with-pair "\"")))
+  ;; Navigation
+  ("f" sp-forward-sexp )
+  ("b" sp-backward-sexp)
+  ("u" sp-backward-up-sexp)
+  ("d" sp-down-sexp)
+  ("p" sp-backward-down-sexp)
+  ("n" sp-up-sexp)
+  ;; Kill/copy
+  ("w" sp-copy-sexp)
+  ("k" sp-kill-sexp)
+  ;; Misc
+  ("t" sp-transpose-sexp)
+  ("j" sp-join-sexp)
+  ("s" sp-split-sexp)
+  ("c" sp-convolute-sexp)
+  ("i" sp-indent-defun)
+  ;; Depth changing
+  ("R" sp-splice-sexp)
+  ("r" sp-splice-sexp-killing-around)
+  ("<up>" sp-splice-sexp-killing-backward)
+  ("<down>" sp-splice-sexp-killing-forward)
+  ;; Barfing/slurping
+  ("<right>" sp-forward-slurp-sexp)
+  ("<left>" sp-forward-barf-sexp)
+  ("C-<left>" sp-backward-barf-sexp)
+  ("C-<right>" sp-backward-slurp-sexp))
 
 ;; counsel
 (use-package counsel
