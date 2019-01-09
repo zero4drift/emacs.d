0daab3062179aa2d4f4a9be290a24787f4a74be2
zero4drift <fang0052@e.ntu.edu.sg>
2019-01-09 13:38:07 +0800

    Add helper functions for ccls(callee, caller, vars, base, derived...)

--- init.el
+++ init.el
@@ -482,6 +482,58 @@
   (push 'company-lsp company-backends))
 
 (use-package ccls
+  :init
+  (defun ccls/callee () (interactive)
+	 (lsp-ui-peek-find-custom "$ccls/call" '(:callee t)))
+  (defun ccls/caller () (interactive)
+	 (lsp-ui-peek-find-custom "$ccls/call"))
+  (defun ccls/vars (kind)
+    (lsp-ui-peek-find-custom "$ccls/vars" `(:kind ,kind)))
+  (defun ccls/base (levels)
+    (lsp-ui-peek-find-custom "$ccls/inheritance" `(:levels ,levels)))
+  (defun ccls/derived (levels)
+    (lsp-ui-peek-find-custom "$ccls/inheritance" `(:levels ,levels :derived t)))
+  (defun ccls/member (kind) (interactive)
+	 (lsp-ui-peek-find-custom "$ccls/member" `(:kind ,kind)))
+
+  ;; References w/ Role::Role
+  (defun ccls/references-read () (interactive)
+	 (lsp-ui-peek-find-custom
+	  "textDocument/references"
+	  (plist-put
+	   (lsp--text-document-position-params) :role 8)))
+
+  ;; References w/ Role::Write
+  (defun ccls/references-write ()
+    (interactive)
+    (lsp-ui-peek-find-custom
+     "textDocument/references"
+     (plist-put
+      (lsp--text-document-position-params) :role 16)))
+
+  ;; References w/ Role::Dynamic bit (macro expansions)
+  (defun ccls/references-macro () (interactive)
+	 (lsp-ui-peek-find-custom
+	  "textDocument/references"
+	  (plist-put
+	   (lsp--text-document-position-params) :role 64)))
+
+  ;; References w/o Role::Call bit (e.g. where functions are taken addresses)
+  (defun ccls/references-not-call () (interactive)
+	 (lsp-ui-peek-find-custom
+	  "textDocument/references"
+	  (plist-put
+	   (lsp--text-document-position-params) :excludeRole 32)))
+  ;; ccls/vars ccls/base ccls/derived ccls/members have a parameter while others are interactive.
+  ;; (ccls/base 1) direct bases
+  ;; (ccls/derived 1) direct derived
+  ;; (ccls/member 2) => 2 (Type) => nested classes / types in a namespace
+  ;; (ccls/member 3) => 3 (Func) => member functions / functions in a namespace
+  ;; (ccls/member 0) => member variables / variables in a namespace
+  ;; (ccls/vars 1) => field
+  ;; (ccls/vars 2) => local variable
+  ;; (ccls/vars 3) => field or local variable. 3 = 1 | 2
+  ;; (ccls/vars 4) => parameter
   :custom
   (ccls-executable "~/github/ccls/Release/ccls")
   (ccls-args '("--log-file=/tmp/ccls.log"))
